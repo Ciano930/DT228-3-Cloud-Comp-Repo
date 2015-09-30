c65be23c7cfabd3f6e1b1627128e2e6a36cfa7b2
ciano930 <ciano930@live.de>
2015-09-30 15:31:30 +0000

    Added some simple instruction text

--- labs/lab3/PalindromePython.py
+++ labs/lab3/PalindromePython.py
@@ -1,5 +1,6 @@
+print("Hi please enter a string")
 word = input()# takes in user entry
-
 word = word.replace(" ", "")#removes any spaces
 word = word.lower()#lowers the case so it can run the Oxo etc
+print("VVV This palindrome is VVV ")#just added some nice text
 print(word == word[::-1])#checks and prints a boolean logic on whether or not the variable is the same when reversed

8d49feb2dd9cf332158efe7421e9f7c0adc8b8e4
ciano930 <ciano930@live.de>
2015-09-30 15:27:23 +0000

    Now the program can alter strings to be lower case and so is not caps sensitive

--- labs/lab3/PalindromePython.py
+++ labs/lab3/PalindromePython.py
@@ -1,4 +1,5 @@
 word = input()# takes in user entry
 
 word = word.replace(" ", "")#removes any spaces
-print(word == word[::-1])
+word = word.lower()#lowers the case so it can run the Oxo etc
+print(word == word[::-1])#checks and prints a boolean logic on whether or not the variable is the same when reversed
